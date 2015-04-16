53ede24685fe6e9feb156b38d94b563ded5753a1
Alon Amster <m@ster.am>
2015-04-16 20:18:48 +0300

    Created a debug-only version This is a bit hack-ish. It depends on the original version so that it is loaded after it. This package unsets the ol namespace in order to avoid namespace conflicts that would prevent the debug version from being exported and then loads the debug version. It is registered as `debugOnly`, so it can be installed alongside the original package and the original version of OpenLayers should be available for production.
    In order to install it:
    1. clone the package repo to the packages subdirectory of your project (or symlink to a local clone)
    2. switch to the `debug_version` branch.
    3. issue `meteor add alon:ol3-debug`.
    meteor add alon:ol3-debug

--- package.js
+++ package.js
@@ -1,14 +1,16 @@
 Package.describe({
   summary: "An open-source JavaScript library to load, display and render maps from multiple sources on the web",
   version: "3.4.0",
-  name: "alon:ol3",
-  git: "https://github.com/masteram/meteor-ol3.git"
+  name: "alon:ol3-debug",
+  git: "https://github.com/masteram/meteor-ol3.git",
+  debugOnly: true
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.0');
   api.use("mizzao:build-fetcher@0.2.0");
-  api.addFiles(['ol3.fetch.json'], 'client');
+  api.use("alon:ol3@3.4.0");
+  api.addFiles(['reset_ol.js','ol3.fetch.json'], 'client');
 });
 
 Package.onTest(function(api) {

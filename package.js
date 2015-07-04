eb9cc8a16afabbee9e0dd42d56ea311bef1a3005
Alon Amster <m@ster.am>
2015-07-04 15:09:33 +0300

    Release v3.7.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  summary: "An open-source JavaScript library to load, display and render maps from multiple sources on the web",
-  version: "3.6.0",
+  summary: "OpenLayers 3: an open-source library to render maps from multiple sources on the web",
+  version: "3.7.0",
   name: "alon:ol3",
   git: "https://github.com/masteram/meteor-ol3.git"
 });
